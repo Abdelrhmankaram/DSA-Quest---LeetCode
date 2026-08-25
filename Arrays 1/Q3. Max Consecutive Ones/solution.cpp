99935287b8a33bc298e2c133857a2b8726aeeafe
Abdelrhmankaram <abdelrhmankaram171@gmail.com>
2026-08-26 00:26:47 +0300

    Add initial implementations for array manipulation problems

---
+++ Arrays 1/Q3. Max Consecutive Ones/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans=0, cnt=0;
+        for(int i = 0 ; i < nums.size() ; i++) {
+            nums[i] == 1 ? cnt++ : cnt=0;
+            ans = max(ans, cnt);
+        }
+        return ans;
+    }
+};
